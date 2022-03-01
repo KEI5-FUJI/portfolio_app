6675c9f41f66592acfdde88b6034dd2c61b551e6
keigo <biolemon.1010@gmail.com>
2022-03-01 15:39:16 +0900

    stop login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  devise_scope :user do
+    root 'devise/registrations#new'
+    get 'login', to: 'devise/sessions#new', as: :login
+    post 'login', to: 'devise/sessions#create', as: :new_login
+    delete 'signout', to: 'devise/sessions#destroy', as: :logout
+  end
 end
